4532a5165629681fc9f749d6505916758e5c06cc
robert <robert.wojcik.programista@gmail.com>
2024-04-09 18:44:20 +0200

    Empty framework structure

---
+++ App/Config.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * PHP version 7.0
+ */
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'your-database-host';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'your-database-name';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'your-database-user';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = 'your-database-password';
+
+    /**
+     * Show or hide error messages on screen
+     * @var boolean
+     */
+    const SHOW_ERRORS = true;
+
+    /**
+     * Secret key for hashing
+     * @var boolean
+     */
+    const SECRET_KEY = 'your-secret-key';
+
+    /**
+     * Mailgun API key
+     *
+     * @var string
+     */
+    const MAILGUN_API_KEY = 'your-mailgun-api-key';
+
+    /**
+     * Mailgun domain
+     *
+     * @var string
+     */
+    const MAILGUN_DOMAIN = 'your-mailgun-domain';
+}
