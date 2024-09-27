dafdd65067e6bfb375b41eeea94e181f3939d487
robert <robert.wojcik.programista@gmail.com>
2024-09-27 21:52:03 +0200

    saving-income-to-db

---
+++ App/Controllers/Incomes.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Flash;
+use App\Models\User;
+use PDO;
+use App\Auth;
+use Core\View;
+use App\Models\Income;
+
+class Incomes extends \Core\Controller
+{
+    public function newAction()
+    {
+//        $loggedUser = Auth::getUser();
+//        var_dump($loggedUser);
+//        $userId = $loggedUser->id;
+
+        $categories = (new \App\Models\Income)->getCategories();
+        View::renderTemplate('Items/income.html', [
+            'categories' => $categories
+        ]);
+    }
+    public function createAction()
+    {
+        $income = new Income($_POST);
+
+        if ($income->save())
+        {
+            Flash::addMessage('Income added successfully');
+
+            View::renderTemplate('Home/index.html');
+        }
+        else
+        {
+            Flash::addMessage('Failed to add income');
+
+            View::renderTemplate('Home/index.html');
+        }
+    }
+}
--- App/Models/Income.php
+++ App/Models/Income.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use PDO;
 use \Core\View;
+use PDOException;
 
 class Income extends \Core\Model
 {
@@ -12,4 +13,45 @@ class Income extends \Core\Model
     private $date;
     private $comment;
 
+    public function getCategories()
+    {
+        $sql = 'SELECT id, name FROM incomes_category_assigned_to_users
+                WHERE user_id = 1';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        //$stmt->bindValue(':token_hash', $this->token_hash, PDO::PARAM_STR);
+
+        $stmt->execute();
+        $categories = $stmt->fetchAll();
+        return $categories;
+    }
+    public function save()
+    {
+        $this->category = $_POST['category'];
+        $this->amount = $_POST['amount'];
+        $this->date = $_POST['date'];
+        $this->comment = $_POST['comment'];
+
+        $sql = 'INSERT INTO incomes (user_id, income_category_assigned_to_user_id, 
+                           amount, date_of_income, income_comment) VALUES (
+                            :user_id, :income_category_assigned_to_user_id,
+                            :amount, :date_of_income, :income_comment
+                           )';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':user_id', 1, PDO::PARAM_STR);
+        $stmt->bindValue(':income_category_assigned_to_user_id', $this->category, PDO::PARAM_STR);
+        $stmt->bindValue(':amount', $this->amount, PDO::PARAM_STR);
+        $stmt->bindValue(':date_of_income', $this->date, PDO::PARAM_STR);
+        $stmt->bindValue(':income_comment', $this->comment, PDO::PARAM_STR);
+
+        try {
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }
